89a7d6543219a7ab915d121e15ff8c7b7b083cb8
Abizrh <abizarah857@gmail.com>
2025-05-02 12:38:25 +0700

    🐛 fix (users): Temporarily disable auth middleware in users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,22 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   getUsers,
   getUserById,
   updateUser,
-  deleteUser
-} = require('../controllers/userController');
-const { protect, authorize } = require('../middleware/authMiddleware');
+  deleteUser,
+} = require("../controllers/userController");
+const { protect, authorize } = require("../middleware/authMiddleware");
 
-router.use(protect);
-router.use(authorize('admin'));
+// router.use(protect);
+// router.use(authorize("admin"));
 
-router.route('/')
-  .get(getUsers);
+router.route("/").get(getUsers);
 
-router.route('/:id')
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
+
